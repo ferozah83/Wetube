03542e1955eafc526f57634a5343596eaade648e
Mankyun CHOI <ferozah83@gmail.com>
2019-09-12 16:54:02 +1200

    Add middleware(cookieParser, bodyParser, helmet, morgan)

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-Parser";
+import bodyParser from "body-Parser";
+
 const app = express();
 
 const PORT = 4000;
@@ -9,12 +14,11 @@ const handleListening = () => console.log(`Listening on: http://localhost:${PORT
 
 const handleProfile = (req, res) => res.send("You are on my profile");
 
-const betweenHome = (req, res ,next) => { 
-  console.log("###Between###"); 
-  next();
-};
-
-app.use(betweenHome);
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(helmet());
+app.use(morgan("dev"));
 
 app.get('/', handleHome);
 
