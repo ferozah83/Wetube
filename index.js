6b4dcc358188cf1c4ba3087f7b5d2ddf3c49a733
Mankyun CHOI <ferozah83@gmail.com>
2019-09-12 15:48:30 +1200

    npm install @babel/node  npm install @babel/preset-env  npm install @babel/core  npm install nodemon -D  Add .babelrc  Add middleware

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("Hi!");
+import express from "express";
+const app = express();
+
+const PORT = 4000;
+
+const handleHome = (req, res) => res.send("Hello. This is MK's side project");
+
+const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
+
+const handleProfile = (req, res) => res.send("You are on my profile");
+
+const betweenHome = (req, res ,next) => { 
+  console.log("###Between###"); 
+  next();
+};
+
+app.use(betweenHome);
+
+app.get('/', handleHome);
+
+app.get('/profile', handleProfile);
+
+app.listen(PORT,handleListening);
