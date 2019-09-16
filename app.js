d9a3f332a63e91274b6fb155255014fecf49fc57
Mankyun CHOI <ferozah83@gmail.com>
2019-09-16 23:18:36 +1200

    Add Controller

--- app.js
+++ app.js
@@ -3,13 +3,12 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-Parser";
 import bodyParser from "body-Parser";
-import { userRouter } from "./router";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
 
 const app = express();
-
-const handleHome = (req, res) => res.send("Hello. This is MK's side project");
-
-const handleProfile = (req, res) => res.send("You are on my profile");
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -17,10 +16,9 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.get('/', handleHome);
-
-app.get('/profile', handleProfile);
-
-app.use("/user", userRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.changePassword, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
