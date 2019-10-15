f0e0ef29c9be0dd96499923fbf8109c8b83f518f
Mankyun CHOI <ferozah83@gmail.com>
2019-10-15 17:22:47 +1300

    Add and Modify user, video Controllers

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,4 +1,5 @@
 import { videos } from "../db"; 
+import routes from "../routes";
 export const home = (req, res) => res.render("home", { pageTitle: "Home", videos });
 
 export const search = (req, res) => { 
@@ -6,7 +7,16 @@ export const search = (req, res) => {
     res.render("search", { pageTitle: "Search", searchingBy, videos });
 };
 
-export const upload = (req, res) => res.render("upload", { pageTitle: "Upload" });
+export const getUpload = (req, res) =>
+  res.render("upload", { pageTitle: "Upload" });
+
+export const postUpload = (req, res) => {
+  const {
+    body: { file, title, description }
+  } = req;
+  // To Do: Upload and save video
+  res.redirect(routes.videoDetail(324393));
+};
 
 export const videoDetail = (req, res) => res.render("videoDetail", { pageTitle: "Video Detail" });
 
