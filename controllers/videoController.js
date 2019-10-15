96317c472731e8e1c5c5e6fcebe00fe2147bb862
Mankyun CHOI <ferozah83@gmail.com>
2019-10-15 16:25:09 +1300

    Add and Modify Join Controller

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -3,7 +3,7 @@ export const home = (req, res) => res.render("home", { pageTitle: "Home", videos
 
 export const search = (req, res) => { 
     const { query: { term:searchingBy}} = req; // const searchingBy = req.query.term;
-    res.render("search", { pageTitle: "Search", searchingBy}); 
+    res.render("search", { pageTitle: "Search", searchingBy, videos });
 };
 
 export const upload = (req, res) => res.render("upload", { pageTitle: "Upload" });
