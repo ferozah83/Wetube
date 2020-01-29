db5b7b22fc985132b38cb301f47fe1ff020911f0
Mankyun CHOI <ferozah83@gmail.com>
2020-01-30 10:05:58 +1300

    Local Authentication with Passport

--- init.js
+++ init.js
@@ -1,12 +1,16 @@
+import dotenv from "dotenv";
 import "./db";
 import app from "./app";
-import dotenv from "dotenv";
+
 dotenv.config();
+
 import "./models/Video";
+import "./models/Comment";
+import "./models/User";
 
 const PORT = process.env.PORT || 4000;
 
 const handleListening = () =>
-  console.log(`✅ Listening on: http://localhost:${PORT}`);
+  console.log(`✅  Listening on: http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
